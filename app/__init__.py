b07e8dae374ad724ad7005e8bb0b100a094b7ca5
Enigmage <alizaidi0786.az@gmail.com>
2020-08-20 00:16:00 +0530

    extra

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flaskext.markdown import Markdown
 from flask_simplemde import SimpleMDE
+from flask_wtf.csrf import CSRFProtect
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 ma = Marshmallow(app) # Serializer
+csrf = CSRFProtect(app)
 mde = SimpleMDE(app)
 md = Markdown(app)
 
--- config.py
+++ config.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv()
 
-
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
