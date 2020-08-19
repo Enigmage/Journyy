f7d2c5f98fceffd21c7b0d6eb0658570152765af
Enigmage <alizaidi0786.az@gmail.com>
2020-08-20 04:54:47 +0530

    added user table

--- app/models.py
+++ app/models.py
@@ -1,11 +1,24 @@
 from app import db, ma
 from datetime import datetime
 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(100), unique = True)
+    email = db.Column(db.String(200), unique = True)
+    passwd = db.Column(db.String(200))
+    posts = db.relationship('Content', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return f"Username:{self.username}"
+
+
 class Content(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), nullable=False)
     content = db.Column(db.String(10000), nullable=False)
-    timestamp = db.Column(db.DateTime, default = datetime.utcnow)
+    timestamp = db.Column(db.DateTime,default = datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return f"Content id {self.id}"
--- journyy.py
+++ journyy.py
@@ -1,4 +1,9 @@
-from app import app
+from app import app, db
+from app.models import User, Content
+
+@app.shell_context_processor
+def make_context():
+    return {'db':db, 'User':User, 'Content':Content}
 
 
 if __name__=='__main__':
