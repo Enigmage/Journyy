a7966f603f904132ac1c8c2138897d3326c50122
Enigmage <alizaidi0786.az@gmail.com>
2020-08-28 02:06:16 +0530

    better route structure

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,30 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField
+from wtforms import SubmitField, StringField, BooleanField, PasswordField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
 
 class markdownform(FlaskForm):
     submit = SubmitField('Post')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me Logged in')
+    submit = SubmitField('Log In')
+
+class SignUpForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('SignUp')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError("Username already exists!!")
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Email already exists')
+
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,13 @@
 from flask import render_template, request, jsonify, redirect
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, login_required
 from app import app, db
-from app.forms import markdownform
+from app.forms import markdownform, LoginForm
 from app.models import Content, ContentSchema 
 
 content_schema = ContentSchema(many=True)
 
 @app.route('/')
+@login_required
 def index():
     try:
         posts = Content.query.filter_by(user_id=1).order_by(Content.id.desc()).all()
@@ -14,13 +15,26 @@ def index():
     except:
         return 'Data could not be queried !!'
 
+
 @app.route('/post')
+@login_required
 def post():
     form = markdownform()
+    if form.validate_on_submit():
+        try:
+            title = request.form['title']
+            content = request.form['pagedown']
+            sub  = Content(title = title, content = content)
+            db.session.add(sub)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was some problem submitting your post !!!'
+
     return render_template('post.html', form=form)
 
 #---------------------------------API TEST-------------------------------------
-@app.route('/test', methods = ['GET', 'POST'])
+@app.route('/test_send', methods = ['GET', 'POST'])
 def test_send():
     title = request.json['title']
     content = request.json['content']
@@ -32,24 +46,13 @@ def test_send():
     except:
         return 'There was some problem!!'
 
-@app.route('/fetch')
+@app.route('/test_fetch')
 def test_fetch():
     x = Content.query.all()
     y = content_schema.dump(x)
     return jsonify(y)
 #------------------------------------------------------------------------------
 
-@app.route('/submit', methods = ['POST'])
-def submit():
-    try:
-        title = request.form['title']
-        content = request.form['pagedown']
-        sub  = Content(title = title, content = content)
-        db.session.add(sub)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return 'There was some problem submitting your post !!!'
 
 @app.route('/read/<int:id>')
 def read(id):
@@ -69,43 +72,28 @@ def delete(id):
     except:
         return 'Post cannot be deleted'
 
-@app.route('/edit/<int:id>')
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit(id):
     try:
         form = markdownform()
+        if form.validate_on_submit():
+            post = Content.query.get_or_404(id)
+            title = request.form['title']
+            content = request.form['pagedown']
+            post.title = title
+            post.content = content
+            db.session.add(post)
+            db.session.commit()
+            return redirect('/')
         change = Content.query.get_or_404(id)
         return render_template('change.html', change = change, form = form)
     except:
         return 'Post cannot be fetched for editing'
 
-@app.route('/editview/<int:id>', methods = ['POST'])
-def editview(id):
-    try:
-        post = Content.query.get_or_404(id)
-        title = request.form['title']
-        content = request.form['pagedown']
-        post.title = title
-        post.content = content
-        db.session.add(post)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return 'There was some problem updating the post!!!'
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if current_user.is_authenticated:
         return redirect('/')
-    if(request.method == 'POST'):
-        username = request.json['username']
-        password = request.json['password']
-        user_check = User.query.filter_by(username=username).first()
-        if user_check is None or not user_check.check_passwd(password):
-            return 'Invalid credentials'
-        login_user(user_check)
-        return 'Successfully Logged in'
-    return 'Login Form'
-
-
-
-
+    form = LoginForm()
+    return render_template('login.html', form = form)
